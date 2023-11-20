9f1b9a4a17aa60d475bbba7cc4e85bec1a74eeb6
AmolShelke2 <shelkeamol897@gmail.com>
2023-11-20 21:30:12 +0530

    minor style changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ export const Header = () => {
   };
 
   return (
-    <header className="flex flex-col sm:flex-row justify-between sm:items-center py-4 px-4 md:px-14 relative gap-4 sm:gap-0">
+    <header className="flex flex-col lg:flex-row justify-between lg:items-center py-4 px-4 md:px-14 relative gap-8 lg:gap-0">
       <div className="cursor-pointer">
         <img className="" src={logo} alt="" height={150} width={150} />
       </div>
@@ -25,7 +25,7 @@ export const Header = () => {
 
       {/* Navigation Links */}
       <nav
-        className={`md:flex list-none gap-8 ${
+        className={`lg:flex list-none gap-8 ${
           show ? "flex flex-col items-start" : "hidden"
         }`}>
         <HeaderItem name={"Home"} link={"home"} />
