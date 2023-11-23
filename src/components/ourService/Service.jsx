afd10755fb46393410e2160582b1e48a0ac5008f
AmolShelke2 <shelkeamol897@gmail.com>
2023-11-23 20:25:49 +0530

    our services updated

--- src/components/ourService/Service.jsx
+++ src/components/ourService/Service.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { ServiceCard } from "./ServiceCard";
-import webdev from "../../assets/services/web-dev.svg";
-import webdesign from "../../assets/services/web-design.svg";
-import seo from "../../assets/services/seo.svg";
-import mobileDev from "../../assets/services/mobile.svg";
+import creativePng from "../../assets/services/creative.png";
+import cyber from "../../assets/services/cyber.webp";
+import digital from "../../assets/services/digital.png";
+import efficient from "../../assets/services/efficient.png";
+import mobile from "../../assets/services/mobile-app.png";
+import seo from "../../assets/services/seo.png";
 
 export const Service = () => {
   return (
     <div className="w-full h-full 2xl:h-screen" id="services">
       <div className="p-4 lg:p-24">
         <div className="mt-32 pb-10 lg:max-w-[1100px] lg:mx-auto">
-          <h3 className="text-center lowercase text-[40px] leading-[50px] lg:text-[96px] lg:leading-[110px] font-thin">
+          <h3 className="text-center capitalize text-[40px] leading-[50px] lg:text-[96px] lg:leading-[110px] font-thin">
             We've been {""}
             <span className="font-text font-bold">
               winning Awards and Hearts
@@ -18,11 +20,15 @@ export const Service = () => {
             for over 12 years...
           </h3>
         </div>
-        <div className="px-4 lg:px-32 mt-8 flex flex-col justify-center items-center lg:flex-row gap-3 py-8">
-          <ServiceCard logo={webdev} service={"Web Devlopment"} />
-          <ServiceCard logo={webdesign} service={"Designing and dev"} />
-          <ServiceCard logo={seo} service={"SEO & Marketing"} />
-          <ServiceCard logo={mobileDev} service={"Mobile Apps"} />
+        <div
+          className="px-4 lg:px-32 mt-8 lg:max-w-[1000px] lg:mx-auto flex flex-col text-center
+         justify-center items-center sm:grid sm:grid-cols-2 sm:place-items-center py-8">
+          <ServiceCard logo={creativePng} service={"creative Designs"} />
+          <ServiceCard logo={seo} service={"Optimizing seo"} />
+          <ServiceCard logo={efficient} service={"efficient website"} />
+          <ServiceCard logo={digital} service={"digital marketing"} />
+          <ServiceCard logo={mobile} service={"responsive mobile app"} />
+          <ServiceCard logo={cyber} service={"best cyber security"} />
         </div>
       </div>
     </div>
--- src/components/ourService/ServiceCard.jsx
+++ src/components/ourService/ServiceCard.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 
-export const ServiceCard = ({ service, logo, description }) => {
+export const ServiceCard = ({ service, logo }) => {
   return (
-    <div
-      className="w-[320px] h-[300px] py-5 px-4 rounded-[20px] bg-white
-     text-black text-center flex flex-col justify-start items-center gap-4">
-      <img src={logo} height={70} width={70} alt="logo" className="mb-4" />
-      <div>
-        <h4 className="font-text text-lg font-semibold mb-3 tracking-[1.2px]">
-          {service}
-        </h4>
-        <p className="text-[15px] leading-6 tracking-wide">
-          Book a free session And Let's build your ideas with our high skilled
-          team!
-        </p>
+    <div className="w-[304px] h-[60px] mb-[20px] flex items-center gap-2">
+      <div className="rounded-full h-[60px] w-[60px] flex items-center justify-center hover:bg-[#db3847] transition-all duration-1000 p-2">
+        <img src={logo} alt="logo" className="h-[40px] w-[40px]" />
       </div>
+      <p className="text-[20px] leading-[28px] w-[224px] h-[28px] text-start font-text font-light capitalize">
+        {service}
+      </p>
     </div>
   );
 };
